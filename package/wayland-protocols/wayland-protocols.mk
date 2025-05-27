145fafc9753ee3866222b6292ffc831b7ed25b2d
Craig Carnell <craig_carnell@hotmail.com>
2025-05-27 12:36:02 +0100

    Add aarch64

--- package/wayland-protocols/wayland-protocols.mk
+++ package/wayland-protocols/wayland-protocols.mk
@@ -6,6 +6,8 @@
 
 WAYLAND_PROTOCOLS_VERSION = 1.11
 WAYLAND_PROTOCOLS_SITE = http://wayland.freedesktop.org/releases
+#WAYLAND_PROTOCOLS_VERSION = 1.42
+#WAYLAND_PROTOCOLS_SITE = https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/$(WAYLAND_PROTOCOLS_VERSION)/downloads
 WAYLAND_PROTOCOLS_SOURCE = wayland-protocols-$(WAYLAND_PROTOCOLS_VERSION).tar.xz
 WAYLAND_PROTOCOLS_LICENSE = MIT
 WAYLAND_PROTOCOLS_LICENSE_FILES = COPYING
@@ -15,3 +17,6 @@ WAYLAND_PROTOCOLS_INSTALL_TARGET = NO
 WAYLAND_PROTOCOLS_DEPENDENCIES = host-wayland
 
 $(eval $(autotools-package))
+#WAYLAND_PROTOCOLS_CONF_OPTS = -Dtests=false
+
+#$(eval $(meson-package)
--- package/wayland/wayland.mk
+++ package/wayland/wayland.mk
@@ -5,6 +5,7 @@
 ################################################################################
 
 WAYLAND_VERSION = 1.11.0
+# WAYLAND_VERSION = 1.23.1
 WAYLAND_SITE = http://wayland.freedesktop.org/releases
 WAYLAND_SOURCE = wayland-$(WAYLAND_VERSION).tar.xz
 WAYLAND_LICENSE = MIT
@@ -19,6 +20,7 @@ HOST_WAYLAND_DEPENDENCIES = host-pkgconf host-expat host-libffi host-libxml2
 
 # wayland-scanner is only needed for building, not on the target
 WAYLAND_CONF_OPTS = --disable-scanner --with-host-scanner
+# WAYLAND_CONF_OPTS = -Dtests=false -Ddocumentation=false
 
 # Remove the DTD from the target, it's not needed at runtime
 define WAYLAND_TARGET_CLEANUP
@@ -27,4 +29,5 @@ endef
 WAYLAND_POST_INSTALL_TARGET_HOOKS += WAYLAND_TARGET_CLEANUP
 
 $(eval $(autotools-package))
+# $(eval $(meson-package))
 $(eval $(host-autotools-package))
