6bbbb3afe2ebb6c1dd056d12ba526e353b4a0e62
Craig Carnell <1188869+cscd98@users.noreply.github.com>
2026-04-28 14:22:32 +0100

    Update webos userland to v0.0.17

--- package/webos-userland/webos-userland.mk
+++ package/webos-userland/webos-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WEBOS_USERLAND_VERSION = 0.0.10
+WEBOS_USERLAND_VERSION = 0.0.17
 WEBOS_USERLAND_SITE = $(call github,webosbrew,webos-userland,$(WEBOS_USERLAND_VERSION))
 WEBOS_USERLAND_LICENSE = Apache/MIT
 WEBOS_USERLAND_INSTALL_STAGING = YES
