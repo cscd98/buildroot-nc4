3bfc1c87f59dea9b8766102eaa04bac94d375a50
Craig Carnell <1188869+cscd98@users.noreply.github.com>
2026-05-03 21:58:31 +0100

    webOS: support optional wayland version/protocol release for compatibility with newer webOS release

--- package/wayland-protocols/wayland-protocols.mk
+++ package/wayland-protocols/wayland-protocols.mk
@@ -4,9 +4,7 @@
 #
 ################################################################################
 
-#WAYLAND_PROTOCOLS_VERSION = 1.45
 WAYLAND_PROTOCOLS_VERSION = 1.11
-#WAYLAND_PROTOCOLS_SITE = https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/$(WAYLAND_PROTOCOLS_VERSION)/downloads
 WAYLAND_PROTOCOLS_SITE = http://wayland.freedesktop.org/releases
 WAYLAND_PROTOCOLS_SOURCE = wayland-protocols-$(WAYLAND_PROTOCOLS_VERSION).tar.xz
 WAYLAND_PROTOCOLS_LICENSE = MIT
@@ -16,8 +14,12 @@ WAYLAND_PROTOCOLS_INSTALL_TARGET = NO
 # needs wayland-scanner
 WAYLAND_PROTOCOLS_DEPENDENCIES = host-wayland
 
+# webOS 26 uses wayland 1.22 with wayland-protocols 1.33, so use latest
+ifeq ($(shell printf "%s\n" $(BR2_PACKAGE_WAYLAND_VERSION) 1.22.0 | sort -V | head -n1),1.22.0)
+WAYLAND_PROTOCOLS_VERSION = 1.45
+WAYLAND_PROTOCOLS_SITE = https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/$(WAYLAND_PROTOCOLS_VERSION)/downloads
+WAYLAND_PROTOCOLS_CONF_OPTS = -Dtests=false
+$(eval $(meson-package))
+else
 $(eval $(autotools-package))
-
-# for modern wayland-protocols:
-#WAYLAND_PROTOCOLS_CONF_OPTS = -Dtests=false
-#$(eval $(meson-package))
+endif
